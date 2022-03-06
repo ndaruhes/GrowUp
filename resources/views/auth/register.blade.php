53f42fd2e65644818c7d3595d706020f560565f1
Vaness <vaness13337@gmail.com>
2022-03-06 23:12:40 +0700

    Menambahkan register scss kedalam mix dan menambahkan view login + login.css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,59 @@
-@extends('layouts.app')
+@extends("layouts.app")
+
+@section('title', 'Login | GrowUp')
+
+@section('cssExternal')
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    @include('layouts.navbar')
+    <div class="auth-section-container">
+        <div class="row h-100">
+            {{-- LEFT SECTION (FORM) --}}
+            <div class="col h-100 auth-section">
+                {{-- HEADER --}}
+                <div class="header-wrapper">
+                    <h1>Welcome to</h1>
+                    <h1> <b class="red-text">G</b>row<b class="green-text">U</b>p</h1>
+                </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                <div class="form-wrapper">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail2" class="form-label">Name</label>
+                            <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="mb-3 form-check d-flex justify-content-center">
+                            <div role="button" class="red-checkbox-container border bg-light">
+                                <img class="w-100 h-100 d-block d-none" src="{{asset("images/icons/checkbox.png")}}" alt="">
+                                <input type="checkbox" class="form-check-input me-2 custom-check-box d-none" id="exampleCheck1">
                             </div>
+                            <label class="form-check-label" for="exampleCheck1">I Agree with <b class="red-text">Terms</b> and <b class="red-text">Privacy</b></label>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
+                        <button type="submit" class="btn bg-green w-100">Submit</button>
                     </form>
+                </div>
+                <p class="text-center mt-5">Already have an account? <a href=""><b class="red-text">Sign in</b></a></p>
+            </div>
+            {{-- RIGHT SECTION (FORM) --}}
+            <div class="col bg-green auth-section auth-section-right">
+                <div class="image-wrapper d-flex justify-content-center">
+                    <img class="auth-section-image" src="{{asset("images/3dimages/Vectary texture.png")}}" alt="">
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
+
+@section("script")
+    <script src="{{asset("js/customCheckBox.js")}}"></script>
 @endsection
