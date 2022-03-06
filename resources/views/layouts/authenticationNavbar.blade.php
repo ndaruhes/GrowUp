74b1368993e545ac0ee7d485faf079e3c2c23101
Vaness <vaness13337@gmail.com>
2022-03-06 16:16:22 +0700

    Setup

---
+++ resources/views/layouts/authenticationNavbar.blade.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link primary-text-color" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link primary-text-color" href="#">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link primary-text-color" href="#">Course</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link primary-text-color">Contact</a>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.authenticationApp")
+
+@section('title', 'Login | GrowUp')
+
+@section('content')
+    <h1>Halaman Home</h1>
+@endsection
