4765b6c362a4467e97edd952863820f1e85df93f
Vaness <vaness13337@gmail.com>
2022-03-10 22:01:46 +0700

    Add course and mentor table

---
+++ database/migrations/2022_03_10_145041_course.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_name');
+            $table->unsignedBigInteger('mentor_id');
+            //Change temporary table mentor
+            $table->foreign('mentor_id')->references('id')->on('mentor');
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
