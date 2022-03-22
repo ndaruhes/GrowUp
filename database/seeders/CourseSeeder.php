394e183767cddee33321d8d0d1f3bc7dc99d9792
Albert <albertbarnabas10@gmail.com>
2022-03-23 01:26:36 +0700

    Added Course Seeder Mandarin 2 Japanese 4

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -20,9 +20,19 @@ class CourseSeeder extends Seeder
             ['https://i.ibb.co/mHXjxYX/course-1.png', 'Membuat REST API Menggunakan ExpressJS & MongoDB', 'Belajar lebih jauh mengenai pengertian, fungsi, dan cara kerja dari Express JS pada pengembangan aplikasi di sisi back end secara fleksibel.', '450000', '2022-07-24', '2022-08-30', '30', '5', '7', '1'],
             ['https://i.ibb.co/RyPVv6Q/course-2.jpg', 'Full Stack Laravel Development: Framework Terbaik Masa Kini', 'Belajar lebih jauh mengenai pengertian, fungsi, dan cara kerja dari Express JS pada pengembangan aplikasi di sisi back end secara fleksibel.', '350000', '2022-04-29', '2022-05-29', '30', '3', '7', '2'],
             ['https://i.ibb.co/dKPxpsT/course-3.jpg', 'Tailwind CSS: Membuat Tampilan Website Menjadi Cantik', 'Belajar lebih jauh mengenai pengertian, fungsi, dan cara kerja dari Express JS pada pengembangan aplikasi di sisi back end secara fleksibel.', null, '2022-04-29', '2022-05-29', '30', '4', '7', '1'],
-            ['https://i.ibb.co/VCwZNYd/course-4.png', 'PHP & MySQL Pemula: Belajar Dari Dasar Sampai Mahir', 'Belajar lebih jauh mengenai pengertian, fungsi, dan cara kerja dari Express JS pada pengembangan aplikasi di sisi back end secara fleksibel.', '150000', '2022-04-29', '2022-05-29', '30', '5', '7', '2']
+            ['https://i.ibb.co/VCwZNYd/course-4.png', 'PHP & MySQL Pemula: Belajar Dari Dasar Sampai Mahir', 'Belajar lebih jauh mengenai pengertian, fungsi, dan cara kerja dari Express JS pada pengembangan aplikasi di sisi back end secara fleksibel.', '150000', '2022-04-29', '2022-05-29', '30', '5', '7', '2'],
 
             // CHINESE LANGUAGE
+            ['https://i.ibb.co/hcr2G1Y/1.jpg', 'Belajar Bahasa Mandarin dari 0', 'Belajar Basic dari Bahasa Mandarin: Pengucapan, Pendengaran, Penulisan dan Pendengaran', '250000', '2022-08-10', '2022-10-10', '30', '5', '7', '1'],
+            ['https://i.ibb.co/945CdPZ/video-thumbnail-hsk-courses.jpg', 'Belajar HSK 1 - 6 untuk pemula', 'Belajar Bahasa Mandarin untuk keperluan bisnis, sekolah dan sehari-hari menggunakan buku HSK', '400000', '2022-07-08', '2022-12-28', '30', '5', '7', '2'],
+
+            // JAPANESE LANGUAGE
+            ['https://i.ibb.co/D7jwgvv/maxresdefault.jpg', 'Bahasa Jepang 101', 'Cara mudah belajar bahasa jepang mulai dari hiragana sampai katakana untuk kalangan muda sampai tua', '50000', '2022-04-04', '2022-04-24', '30', '4', '7', '1'],
+            ['https://i.ibb.co/dDgf8g5/introduction-to-kanji-thumbnail.png', 'Paket lengkap hiragana, katakana dan kanji untuk pemula', 'Cara mudah belajar bahasa jepang mulai dari hiragana sampai katakana untuk kalangan muda sampai tua', '50000', '2022-05-14', '2022-06-14', '30', '3', '7', '2'],
+            ['https://i.ibb.co/Wp6HJGY/a40b17a495396620a0beae94158380.jpg', 'Belajar Kanji dalam bahasa jepang', 'Cara mudah belajar bahasa jepang mulai dari hiragana sampai katakana untuk kalangan muda sampai tua', '50000', '2022-04-23', '2022-06-23', '30', '4', '7', '1'],
+            ['https://i.ibb.co/wBJqYBD/japanesepod101.jpg', 'Nihongo dalam 1 jam', 'Cara mudah belajar bahasa jepang mulai dari hiragana sampai katakana untuk kalangan muda sampai tua', '50000', '2022-07-05', '2022-06-05', '30', '5', '7', '2'],
+
+
         ];
 
         foreach ($courses as $course) {
--- database/seeders/SessionSeeder.php
+++ database/seeders/SessionSeeder.php
@@ -18,8 +18,7 @@ class SessionSeeder extends Seeder
         $sessions = [
             ['Perkenalan NodeJS (Sintaks, Variabel, Dll)', 'Di pertemuan pertama kita akan berkenalan dengan NodeJS, mempelajari sintaks, dan cara menulis variabel', 'https://meet.google.com/cuw-pkau-iux', 'contoh_microservicesmonolithic (1)-CBUbGV4QSR-20220321044331.pdf', '2022-07-24', '09:30', '1'],
             ['Perkenalan MongoDB (Table, Query)', 'Di pertemuan kedua kita akan berkenalan dengan MongoDB sekaligus belajar membuat table dan menjalankan query', 'https://meet.google.com/bgh-bckj-ghd', 'contoh_microservicesmonolithic-iUf7OFpkHS-20220321044412.pdf', '2022-07-25', '10:30', '1'],
-            ['Perkenalan MongoDB (Table, Query)', 'Di pertemuan kedua kita akan berkenalan dengan MongoDB sekaligus belajar membuat table dan menjalankan query', 'https://meet.google.com/bgh-bckj-ghd', 'contoh_microservicesmonolithic-iUf7OFpkHS-20220321044412.pdf', '2022-07-27', '08:30', '1'],
-            ['Perkenalan MongoDB (Table, Query)', 'Di pertemuan kedua kita akan berkenalan dengan MongoDB sekaligus belajar membuat table dan menjalankan query', 'https://meet.google.com/bgh-bckj-ghd', 'contoh_microservicesmonolithic-iUf7OFpkHS-20220321044412.pdf', '2022-07-30', '12:30', '1']
+            
         ];
 
         foreach ($sessions as $session) {
